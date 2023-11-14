e4887e3876feb3ce25c8996fdbb20f2471f22c2d
Matias Vazquez <matiasvz1913@gmal.com>
2023-11-14 18:39:21 -0300

    preentrega

--- Style.js
+++ Style.js
@@ -1,26 +1,26 @@
-console.log("Conectado")
-
 // Función para calcular pagos en cuotas
-function calcularCuotas(montoTotal, numeroCuotas) {
-    // Verificar si el número de cuotas es válido
-    if (numeroCuotas <= 0) {
-        console.log("El número de cuotas debe ser mayor que cero.");
-        return;
+function calcularCuotas(monto, cuotas) {
+    // Verificar que el número de cuotas sea válido (mayor que cero)
+    if (cuotas <= 0) {
+      console.log("El número de cuotas debe ser mayor que cero.");
+      return;
     }
-
+  
     // Calcular el monto de cada cuota
-    const montoCuota = montoTotal / numeroCuotas;
-
-    // Imprimir los detalles de cada cuota utilizando un bucle for
-    for (let i = 1; i <= numeroCuotas; i++) {
-        console.log(`Cuota ${i}: $${montoCuota.toFixed(2)}`);
+    var montoPorCuota = monto / cuotas;
+  
+    // Imprimir el detalle de las cuotas
+    console.log("Detalle de pagos en cuotas:");
+    for (var i = 1; i <= cuotas; i++) {
+      console.log("Cuota " + i + ": $" + montoPorCuota.toFixed(2));
     }
-}
+  }
+  
+  // Ejemplo de uso
+  var montoTotal = 1000;
+  var numeroDeCuotas = 5;
+  
+  calcularCuotas(montoTotal, numeroDeCuotas);
+  
 
-// Ejemplo de uso
-const montoTotal = 1000; // Monto total a pagar
-const numeroCuotas = 3;  // Número de cuotas
-
-// Llamar a la función para calcular e imprimir las cuotas
-calcularCuotas(montoTotal, numeroCuotas);
 
