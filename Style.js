f19442c712eb4188a8bb7c4c8efe443da44bdfc3
Matias Vazquez <matiasvz1913@gmal.com>
2023-11-14 17:29:09 -0300

    primera pre entrega

--- Style.js
+++ Style.js
@@ -1,15 +1,43 @@
 console.log("Conectado")
 
-let MontoPrincipal = prompt("Ingresa Un Monto");
-let Cuota = prompt("Ingresa nº de Cuotas");
+function calcularPagosEnCuotas(montoPrestamo, tasaInteresAnual, numeroCuotas) {
+    // Calcular la tasa de interés mensual
+    const tasaInteresMensual = tasaInteresAnual / 100 / 12;
 
+    // Calcular el pago mensual utilizando la fórmula de amortización
+    const pagoMensual = montoPrestamo * (tasaInteresMensual * Math.pow(1 + tasaInteresMensual, numeroCuotas)) / (Math.pow(1 + tasaInteresMensual, numeroCuotas) - 1);
 
-function calcularCuota(montoPrincipal,numeroCuotas) {
-    if (numeroCuotas <= 0) {
-        console.log("El número de cuotas debe ser mayor que cero");
-    } else {
-        const tasaPeriodica = tasaInteres / 12; // Suponiendo pagos mensuales
-        const cuota = (montoPrincipal * tasaPeriodica * Math.pow((1 + tasaPeriodica), numeroCuotas)) / (Math.pow((1 + tasaPeriodica), numeroCuotas) - 1);
-        console.log("El pago mensual será: " + cuota);
+    // Imprimir el pago mensual
+    console.log(`El pago mensual es: $${pagoMensual.toFixed(2)}`);
+
+    // Ciclo para mostrar el desglose de pagos mensuales
+    for (let cuota = 1; cuota <= numeroCuotas; cuota++) {
+        // Calcular la parte de interés del pago mensual
+        const interesMensual = montoPrestamo * tasaInteresMensual;
+
+        // Calcular la parte de capital del pago mensual
+        const capitalMensual = pagoMensual - interesMensual;
+
+        // Actualizar el saldo del préstamo
+        montoPrestamo -= capitalMensual;
+
+        // Imprimir el desglose de cada cuota
+        console.log(`\nCuota ${cuota}:`);
+        console.log(`  Pago mensual: $${pagoMensual.toFixed(2)}`);
+        console.log(`  Interés mensual: $${interesMensual.toFixed(2)}`);
+        console.log(`  Capital mensual: $${capitalMensual.toFixed(2)}`);
+        console.log(`  Saldo restante: $${montoPrestamo.toFixed(2)}`);
+
+        // Verificar si es la última cuota
+        if (cuota === numeroCuotas) {
+            console.log("\n¡Última cuota pagada!");
+        }
     }
 }
+
+// Ejemplo de uso
+const montoPrestamo = 1000;  // Monto del préstamo
+const tasaInteresAnual = 5;  // Tasa de interés anual
+const numeroCuotas = 12;  // Número de cuotas
+
+calcularPagosEnCuotas(montoPrestamo, tasaInteresAnual, numeroCuotas);
